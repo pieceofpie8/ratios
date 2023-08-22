acac6ff539a3e59c916d1afbfa41ba2ce6c989b7
Jason M. Dong <jasondonghehe2006@gmail.com>
2023-08-22 17:16:15 -0400

    Saved axios.get function

--- myFunction/index.mjs
+++ myFunction/index.mjs
@@ -1,11 +1,20 @@
-import { XMLParser } from 'fast-xml-parser';
-import { readFileSync } from 'fs';
+import { XMLParser } from "fast-xml-parser";
+import { readFileSync } from "fs";
+import axios from "axios";
 
 export const handler = async (event, context) => {
-    const xmlFile = readFileSync(`${process.cwd()}/treasury.xml`, 'utf8');
-    const parser = new XMLParser();
-    const json = parser.parse(xmlFile);
-    let riskFreeRate = parseFloat(json.feed.entry[0].content["m:properties"]["d:BC_2MONTH"]) / 100;
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
-    return context.logStreamName;
+  // const res = await axios.get("http://172.31.19.0:8181/status");
+  // const res = await axios.get("http://webcode.me/");
+  const res = await axios.get(
+    "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/pages/xml?data=daily_treasury_bill_rates&field_tdr_date_value=2022"
+  );
+  console.log(res.data);
+  const xmlFile = readFileSync(`${process.cwd()}/treasury.xml`, "utf8");
+  const parser = new XMLParser();
+  const json = parser.parse(xmlFile);
+  let riskFreeRate =
+    parseFloat(json.feed.entry[0].content["m:properties"]["d:BC_2MONTH"]) / 100;
+  console.log(riskFreeRate);
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+  return context.logStreamName;
 };
