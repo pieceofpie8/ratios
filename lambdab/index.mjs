5b53e14a176b32903249e4cab6df2db1986e09ff
Jason M. Dong <jasondonghehe2006@gmail.com>
2023-10-17 00:44:03 -0400

    Major Edits for the Code

--- lambdab/index.mjs
+++ lambdab/index.mjs
@@ -1,88 +1,84 @@
 import AWS from "aws-sdk";
+import axios from "axios";
+import { XMLParser } from "fast-xml-parser";
+import { parseString } from "xml2js";
+
 const s3 = new AWS.S3();
 
 export const handler = async (event) => {
-  /*
-    // TODO implement
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify("Hello World!"),
-  };
-  return response;
-  */
   try {
-    // Specify the S3 bucket and file key
+    // context.callbackWaitsForEmptyEventLoop = false;
+    const res = await axios.get(
+      "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/pages/xml?data=daily_treasury_yield_curve&field_tdr_date_value=2023"
+    );
+    // const json = res.data;
+
+    // const parser = new XMLParser();
+    // const json = parser.parse(res);
+
+    let json;
+    parseString(res.data, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      json = result;
+    });
+
+    // Now you can access json.feed.entry
+    // console.log(json.feed.entry[0], "IS THE FIRST ENTRY");
+
+    // console.log(JSON.stringify(json, null, 2), "IS THE STRINGIFY JSON FILE");
+    // console.log(res, " IS THE RESULT OF AXIOS!!!");
+    // console.log(json, " IS THE JSON FILE FOR ANALYSIS!!!");
+    // console.log(res.feed, " IS THE FEED FOR RES");
+    // console.log(json.feed, " IS THE FEED FOR JSON");
+    // console.log(json["entry"][0], "IS THE ENTRY");
+    // console.log();
+
+    // Specify the S3 bucket and file keys
     const bucketName = "ust-ff-jason";
-    const ust1M = "UST_1M.json"; // Replace with your file's path and name
-    const ust1Y = "UST_1Y.json";
-    const ust2M = "UST_2M.json";
-    const ust2Y = "UST_2Y.json";
-    const ust3M = "UST_3M.json";
-    const ust3Y = "UST_3Y.json";
-    const ust4M = "UST_4M.json";
-    const ust5Y = "UST_5Y.json";
-    const ust6M = "UST_6M.json";
-    const ust7Y = "UST_7Y.json";
-    const ust10Y = "UST_10Y.json";
-    const ust20Y = "UST_20Y.json";
-    const ust30Y = "UST_30Y.json";
+    const fileKeys = [
+      "UST_1M.json",
+      "UST_1Y.json",
+      "UST_2M.json",
+      "UST_2Y.json",
+      "UST_3M.json",
+      "UST_3Y.json",
+      "UST_4M.json",
+      "UST_5Y.json",
+      "UST_6M.json",
+      "UST_7Y.json",
+      "UST_10Y.json",
+      "UST_20Y.json",
+      "UST_30Y.json",
+    ];
 
     // Specify S3 parameters
-    const paramsUst1M = { Bucket: bucketName, Key: ust1M };
-    const paramsUst1Y = { Bucket: bucketName, Key: ust1Y };
-    const paramsUst2M = { Bucket: bucketName, Key: ust2M };
-    const paramsUst2Y = { Bucket: bucketName, Key: ust2Y };
-    const paramsUst3M = { Bucket: bucketName, Key: ust3M };
-    const paramsUst3Y = { Bucket: bucketName, Key: ust3Y };
-    const paramsUst4M = { Bucket: bucketName, Key: ust4M };
-    const paramsUst5Y = { Bucket: bucketName, Key: ust5Y };
-    const paramsUst6M = { Bucket: bucketName, Key: ust6M };
-    const paramsUst7Y = { Bucket: bucketName, Key: ust7Y };
-    const paramsUst10Y = { Bucket: bucketName, Key: ust10Y };
-    const paramsUst20Y = { Bucket: bucketName, Key: ust20Y };
-    const paramsUst30Y = { Bucket: bucketName, Key: ust30Y };
+    const s3Promises = fileKeys.map(async (fileKey) => {
+      const params = { Bucket: bucketName, Key: fileKey };
+      const s3Response = await s3.getObject(params).promise();
+      return JSON.parse(s3Response.Body.toString("utf-8"));
+    });
 
-    // Read the JSON file from S3
-    // Parse the JSON data
-    const s3Response1M = await s3.getObject(paramsUst1M).promise();
-    const jsondata1M = JSON.parse(s3Response1M.Body.toString("utf-8"));
-    const s3Response1Y = await s3.getObject(paramsUst1Y).promise();
-    const jsondata1Y = JSON.parse(s3Response1Y.Body.toString("utf-8"));
-    const s3Response2M = await s3.getObject(paramsUst2M).promise();
-    const jsondata2M = JSON.parse(s3Response2M.Body.toString("utf-8"));
-    const s3Response2Y = await s3.getObject(paramsUst2Y).promise();
-    const jsondata2Y = JSON.parse(s3Response2Y.Body.toString("utf-8"));
-    const s3Response3M = await s3.getObject(paramsUst3M).promise();
-    const jsondata3M = JSON.parse(s3Response3M.Body.toString("utf-8"));
-    const s3Response3Y = await s3.getObject(paramsUst3Y).promise();
-    const jsondata3Y = JSON.parse(s3Response3Y.Body.toString("utf-8"));
-    const s3Response4M = await s3.getObject(paramsUst4M).promise();
-    const jsondata4M = JSON.parse(s3Response4M.Body.toString("utf-8"));
-    const s3Response5Y = await s3.getObject(paramsUst5Y).promise();
-    const jsondata5Y = JSON.parse(s3Response5Y.Body.toString("utf-8"));
-    const s3Response6M = await s3.getObject(paramsUst6M).promise();
-    const jsondata6M = JSON.parse(s3Response6M.Body.toString("utf-8"));
-    const s3Response7Y = await s3.getObject(paramsUst7Y).promise();
-    const jsondata7Y = JSON.parse(s3Response7Y.Body.toString("utf-8"));
-    const s3Response10Y = await s3.getObject(paramsUst10Y).promise();
-    const jsondata10Y = JSON.parse(s3Response10Y.Body.toString("utf-8"));
-    const s3Response20Y = await s3.getObject(paramsUst20Y).promise();
-    const jsondata20Y = JSON.parse(s3Response20Y.Body.toString("utf-8"));
-    const s3Response30Y = await s3.getObject(paramsUst30Y).promise();
-    const jsonData30Y = JSON.parse(s3Response30Y.Body.toString("utf-8"));
-
-    // Now you can work with the JSON data as needed
-    /* console.log("JSON data:", jsondata1M, "\n", jsondata1Y, "\n", jsondata2M, "\n",
-      jsondata2Y, "\n", jsondata3M, "\n", jsondata3Y, "\n", jsondata4M, "\n",
-      jsondata5Y, "\n", jsondata6M, "\n", jsondata7Y, "\n", jsondata10Y, "\n",
-      jsondata20Y, "\n", jsonData30Y
-    );
-    */
-
+    // Wait for all S3 reads to complete
+    const [
+      jsondata1M,
+      jsondata1Y,
+      jsondata2M,
+      jsondata2Y,
+      jsondata3M,
+      jsondata3Y,
+      jsondata4M,
+      jsondata5Y,
+      jsondata6M,
+      jsondata7Y,
+      jsondata10Y,
+      jsondata20Y,
+      jsonData30Y,
+    ] = await Promise.all(s3Promises);
     let stk = [];
-    // ff("UST_1M", "d:BC_1MONTH", jsondata1M, stk, ust1M);
-    function ff(ffTerm, ustTerm, json, stk, ust) {
-      const ffJsonMarketData = ust;
+    ff("UST_1M", "d:BC_1MONTH", json, stk, jsondata1M);
+    function ff(ffTerm, ustTerm, json, stk, ffJsonMarketData) {
       const eodData = ffJsonMarketData.results.history[0].eoddata;
       const equityInfo = ffJsonMarketData.results.history[0].equityinfo;
       const Key = ffJsonMarketData.results.history[0].key;
@@ -105,16 +101,28 @@ export const handler = async (event) => {
       Key.ffUrl = URL;
       let startDate, endDate;
 
+      console.log("HEY!!!");
+
       let dd =
-        json.feed.entry[json.feed.entry.length - 1].content["m:properties"][
-          "d:NEW_DATE"
-        ];
-      dd = dd.substring(0, dd.length - 9);
+        json.feed.entry[json.feed.entry.length - 1].content[0][
+          "m:properties"
+        ][0]["d:NEW_DATE"];
+      let dateValue = dd && dd[0] && dd[0]["_"];
+      dateValue = dateValue.substring(0, dateValue.length - 9);
+
+      let bc1MonthProperty =
+        json.feed.entry[json.feed.entry.length - 1].content[0][
+          "m:properties"
+        ][0]["d:BC_1MONTH"];
+      let bc1MonthValue =
+        bc1MonthProperty && bc1MonthProperty[0] && bc1MonthProperty[0]["_"];
+      let bc1MonthInt = bc1MonthValue ? parseFloat(bc1MonthValue) : null;
+
       eodData.push({
         date: dd,
-        close: json.feed.entry[0].content["m:properties"][ustTerm],
+        close: bc1MonthInt,
       });
-      endDate = dd;
+      endDate = dateValue;
 
       ffJsonMarketData.results.history[0].start = startDate;
       ffJsonMarketData.results.history[0].end = endDate;
@@ -122,18 +130,20 @@ export const handler = async (event) => {
       // Putting in eodData in reverse order
       while (stk.length) {
         let hehe = stk[stk.length - 1];
-        ust.results.history[0].eoddata.push(hehe);
+        ffJsonMarketData.results.history[0].eoddata.push(hehe);
         stk.pop();
       }
-      const sortedUst = ust.results.history[0].eoddata.sort((a, b) => {
-        if (a.date < b.date) return 1;
-        if (a.date > b.date) return -1;
-        return 0;
-      });
+      const sortedUst = ffJsonMarketData.results.history[0].eoddata.sort(
+        (a, b) => {
+          if (a.date < b.date) return 1;
+          if (a.date > b.date) return -1;
+          return 0;
+        }
+      );
 
       console.log(ffTerm, ustTerm);
       console.log("\n");
-      console.log(JSON.stringify(sortedUst1M));
+      console.log(JSON.stringify(sortedUst));
       console.log("\n");
     }
     return {
